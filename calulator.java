a9717163a09d799137cbecee1061471fc0895094
gaurav <nimbalkargaurav100@gmail.com>
2025-02-20 14:55:11 +0530

    third commit

--- calulator.java
+++ calulator.java
@@ -10,5 +10,6 @@ public class calulator {
         System.out.println("Addition of a and b is :" + (a + b));
         System.out.println("Subtraction of a and b is :" + (a - b));
          System.out.println("Divide of a and b is :" + (a / b));
+         System.out.println("Multiplication of a and b is :" + (a * b));
     }
 }
