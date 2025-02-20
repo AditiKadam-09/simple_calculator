a3a75cd1162dc8a4c1718ae39f7c869f1a351af3
Aditi Kadam <aditikadam2005@gmail.com>
2025-02-20 14:37:11 +0530

    added the first program

---
+++ calulator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class calulator {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number :");
+        int a = input.nextInt();
+        System.out.print("Enter second number :");
+        int b = input.nextInt();
+        System.out.println("Addition of a and b is :" + (a + b));
+        System.out.println("Subtraction of a and b is :" + (a - b));
+    }
+}
